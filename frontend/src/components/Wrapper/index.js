0ef2b93a8875abd808f169f168c285edd3460cf9
Yamsmdj <yamine.mdj@gmail.com>
2024-06-03 02:28:41 +0200

    _

--- frontend/src/components/Wrapper/index.js
+++ frontend/src/components/Wrapper/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
-const index = ({children}) => {
-    return (
-        <div>
-            {children}
-        </div>
-    );
+const index = ({ children }) => {
+  return (
+    <div>
+      <Navbar />
+      {children}
+      <Footer />
+    </div>
+  );
 };
 
 export default index;
